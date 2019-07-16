953c569d9f77061bb34934ad8d8a1d1b0c70efe6
Jason Jiang <jjiang@bird.co>
2019-07-16 14:19:19 -0700

    new algo

--- chatbot_handler.py
+++ chatbot_handler.py
@@ -2,6 +2,8 @@
 import json
 import random
 import markovgen
+import markovify
+import numpy as np
 
 import requests
 from flask import request, Flask
@@ -9,6 +11,9 @@ from flask import request, Flask
 app = Flask(__name__)
 
 token = 'EAAgZAanyls4sBAN12HSpOt7txZABocVsuw1GchwcUNUIRXayXpmXudAa1poT0bnsjI5LZBByvItdJv2yGLA6KStAwMguTAvlm5ZBGK56dSv9oLkCZCRcxKtQ0j21pg4SAUShyeK9wbqdWrtutoybmLuG3iw469ZCucrGG3kI9ko1ZAlBZC9q8C59'  # noqa
+
+TEXT_AVERAGE_LENGTH = 22
+text = None
 
 file_ = open('messages.txt')
 markov = markovgen.Markov(file_)
@@ -25,8 +30,34 @@ def serve():
     return 'the'
 
 
+# def generate_speech():
+#     return markov.generate_markov_text()
+
 def generate_speech():
-    return markov.generate_markov_text()
+    # Get raw text as string.
+    global text
+    if not text:
+        with open("messages.txt") as f:
+            text = f.read()
+
+    text_models = []
+    for i in range(6):
+        # Build the models
+        text_models.append(markovify.Text(text, state_size=i))
+
+    # Use a random distribution to figure out how many sentences to generate
+    num_sentences = max(1, int(round(np.random.normal(1, 0.5, 1)[0], 0)))
+
+    sentences = []
+    for i in range(num_sentences):
+        # Make a random choice on which model to use
+        chosen_text_model = text_models[random.randint(0, 5)]
+        # Generate sentences certain length
+        sentences.append(chosen_text_model.make_short_sentence(max_chars=TEXT_AVERAGE_LENGTH*7, min_chars=TEXT_AVERAGE_LENGTH/3, tries=1000))
+
+    sentences = " ".join(sentences)
+
+    return sentences
 
 
 @app.route('/receive', methods=['POST'])
